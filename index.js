88c75bfdca165404d8b5d5b56f586ae11e8257ee
glushkina1 <40953265+glushkina1@users.noreply.github.com>
2021-08-14 02:26:33 +0300

    init_push

--- index.js
+++ index.js
@@ -4,6 +4,6 @@
 import React from "react";
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-import App from "./src/Appwrapper";
+import AppWrapper from "./src/Appwrapper";
 
 AppRegistry.registerComponent(appName, () => AppWrapper);
